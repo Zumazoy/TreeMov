2b62d226e6cbf486c63f84983689072f1aed8718
TTPPOOAAMM <maxvashencko74@gmail.com>
2026-02-17 14:38:47 +0300

    .

--- lib/core/themes/app_colors.dart
+++ lib/core/themes/app_colors.dart
@@ -1,3 +1,4 @@
+// lib/core/themes/app_colors.dart
 import 'package:flutter/material.dart';
 
 class AppColors {
@@ -14,20 +15,25 @@ class AppColors {
   static const Color eventTap = Color(0xFFF0F0FF);
   static const Color lightGrey = Color(0xFFE0E0E0);
 
+  // Standard Colors
   static const Color white = Colors.white;
+  static const Color black = Colors.black;
   static const Color grey = Colors.grey;
   static const Color shadow = Color(0x1A000000);
   static const Color dialogBarrier = Color(0x80000000);
   static const Color grayFieldText = Color.fromARGB(255, 47, 33, 62);
 
+  // Directory Colors (Light)
   static const Color directoryTextSecondary = Color(0xFF9CA3AF);
   static const Color directoryBorder = Color(0xFFE5E7EB);
   static const Color directoryAvatarBorder = Color(0xFFD7D6FF);
   static const Color directoryAvatarBackground = Color(0xFFE5E7EB);
 
+  // Notes Colors (Light)
   static const Color notesBackground = Color(0xFFF9FAFB);
   static const Color notesDarkText = Color(0xFF2F213E);
 
+  // Category Colors (Light)
   static const Color categoryBehaviorBg = Color(0xFFDBEAFE);
   static const Color categoryBehaviorText = Color(0xFF1E40AF);
   static const Color categoryStudyBg = Color(0xFFDCFCE7);
@@ -37,6 +43,7 @@ class AppColors {
   static const Color categoryGeneralBg = Color(0xFFFFEDD5);
   static const Color categoryGeneralText = Color(0xFF9A3412);
 
+  // Stats Colors (Light)
   static const Color statsTotalBg = Color(0xFFF0FDF4);
   static const Color statsTotalBorder = Color(0xFFBBF7D0);
   static const Color statsTotalText = Color(0xFF15803D);
@@ -48,4 +55,26 @@ class AppColors {
   static const Color statsTodayBg = Color(0xFFFAF5FF);
   static const Color statsTodayBorder = Color(0xFFE9D5FF);
   static const Color statsTodayText = Color(0xFF6D28D9);
+
+  // Dark Theme Colors
+  static const Color darkBackground = Color(0xFF121212);
+  static const Color darkSurface = Color(0xFF1E1E1E);
+  static const Color darkCard = Color(0xFF242424);
+  static const Color darkText = Color(0xFFE0E0E0);
+  static const Color darkTextSecondary = Color(0xFF9E9E9E);
+
+  // Dark Theme Component Overrides
+  static const Color darkEventTap = Color(0xFF2F2C54);
+  static const Color darkNotesBackground = darkBackground;
+  static const Color darkNotesDarkText = darkText;
+
+  // Dark Theme Category Overrides
+  static const Color darkCategoryBehaviorBg = Color(0xFF1E3A8A);
+  static const Color darkCategoryBehaviorText = Color(0xFF93C5FD);
+  static const Color darkCategoryStudyBg = Color(0xFF065F46);
+  static const Color darkCategoryStudyText = Color(0xFF6EE7B7);
+  static const Color darkCategoryParentsBg = Color(0xFF581C87);
+  static const Color darkCategoryParentsText = Color(0xFFDDA6FF);
+  static const Color darkCategoryGeneralBg = Color(0xFF9A3412);
+  static const Color darkCategoryGeneralText = Color(0xFFFFCC80);
 }
