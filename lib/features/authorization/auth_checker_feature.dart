bb502ee58925a663e5f6a48853324a9748187a0a
Zumazoy <solovevstepan878@gmail.com>
2026-02-18 17:25:13 +0300

    Фикс экрана авторизации

--- lib/features/authorization/auth_checker_feature.dart
+++ lib/features/authorization/auth_checker_feature.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:treemov/app/di/di.config.dart';
 import 'package:treemov/core/storage/secure_storage_repository.dart';
 import 'package:treemov/features/authorization/presentation/screens/auth_checker_screen.dart';
-import 'package:treemov/features/authorization/presentation/screens/entrance_screen.dart';
+import 'package:treemov/features/authorization/presentation/screens/entrance_kid_screen.dart';
 
 class AuthCheckerFeature {
   static Widget createAuthChecker() {
@@ -12,6 +12,6 @@ class AuthCheckerFeature {
   }
 
   static Widget createEntranceScreen() {
-    return const EntranceScreen();
+    return const EntranceKidScreen();
   }
 }
