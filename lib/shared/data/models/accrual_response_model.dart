93eef4fccdf15c8339d7cd9b88756fafb4076932
Zumazoy <solovevstepan878@gmail.com>
2026-03-04 12:44:28 +0300

    createdAt в accrual

--- lib/shared/data/models/accrual_response_model.dart
+++ lib/shared/data/models/accrual_response_model.dart
@@ -6,6 +6,7 @@ class AccrualResponseModel extends BaseResponseModel {
   final int? amount;
   final String? comment;
   final String? category;
+  final String? createdAt;
   final TeacherResponseModel? teacher;
   final StudentResponseModel? student;
 
@@ -14,6 +15,7 @@ class AccrualResponseModel extends BaseResponseModel {
     required this.amount,
     required this.comment,
     required this.category,
+    required this.createdAt,
     required this.teacher,
     required this.student,
   });
@@ -24,6 +26,7 @@ class AccrualResponseModel extends BaseResponseModel {
       amount: json['amount'],
       comment: json['comment'],
       category: json['category'],
+      createdAt: json['created_at'],
       teacher: json['teacher'] != null
           ? TeacherResponseModel.fromJson(json['teacher'])
           : null,
