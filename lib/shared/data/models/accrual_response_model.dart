7def00f13749f56f88d1dca5a6c8b9135a871da3
Zumazoy <solovevstepan878@gmail.com>
2026-03-04 12:09:20 +0300

    accrual будет таким и .

--- lib/shared/data/models/accrual_response_model.dart
+++ lib/shared/data/models/accrual_response_model.dart
@@ -1,57 +1,35 @@
 import 'package:treemov/shared/data/models/student_response_model.dart';
 import 'package:treemov/shared/data/models/teacher_response_model.dart';
+import 'package:treemov/shared/domain/models/base_response_model.dart';
 
-class AccrualResponseModel {
-  final int id;
-  final int amount;
-  final String comment;
+class AccrualResponseModel extends BaseResponseModel {
+  final int? amount;
+  final String? comment;
+  final String? category;
   final TeacherResponseModel? teacher;
   final StudentResponseModel? student;
-  final String category;
 
   AccrualResponseModel({
-    required this.id,
+    required super.baseData,
     required this.amount,
     required this.comment,
-    this.teacher,
-    this.student,
     required this.category,
+    required this.teacher,
+    required this.student,
   });
 
   factory AccrualResponseModel.fromJson(Map<String, dynamic> json) {
-    TeacherResponseModel? parseTeacher() {
-      try {
-        if (json['teacher'] != null && json['teacher'] is Map) {
-          return TeacherResponseModel.fromJson(
-            Map<String, dynamic>.from(json['teacher']),
-          );
-        }
-      } catch (e) {
-        print('⚠️ Ошибка парсинга teacher: $e');
-      }
-      return null;
-    }
-
-    StudentResponseModel? parseStudent() {
-      try {
-        if (json['student'] != null && json['student'] is Map) {
-          return StudentResponseModel.fromJson(
-            Map<String, dynamic>.from(json['student']),
-          );
-        }
-      } catch (e) {
-        print('⚠️ Ошибка парсинга student: $e');
-      }
-      return null;
-    }
-
     return AccrualResponseModel(
-      id: json['id'] ?? 0,
-      amount: json['amount'] ?? 0,
-      comment: json['comment']?.toString() ?? '',
-      teacher: parseTeacher(),
-      student: parseStudent(),
-      category: json['category']?.toString() ?? '',
+      baseData: json.baseData,
+      amount: json['amount'],
+      comment: json['comment'],
+      category: json['category'],
+      teacher: json['teacher'] != null
+          ? TeacherResponseModel.fromJson(json['teacher'])
+          : null,
+      student: json['student'] != null
+          ? StudentResponseModel.fromJson(json['student'])
+          : null,
     );
   }
 }
